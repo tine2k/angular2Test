f9c46b83400c7e74ca32d62b4e386b57774ce9ff
tine2k <martin.maier.moessner@gmail.com>
2015-12-09 18:05:26 +0100

    service example

---
+++ services.js
@@ -0,0 +1,15 @@
+var MyService = function () {
+};
+
+MyService.prototype.query = function (count) {
+    return [{
+        name: 'Peter',
+        age: 19
+    }, {
+        name: 'Martin',
+        age: 23
+    }, {
+        name: 'Sarah',
+        age: 32
+    }];
+}
