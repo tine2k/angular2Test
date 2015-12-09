aefe35b438759bf5f9cc78b5af09f919bec67266
tine2k <martin.maier.moessner@gmail.com>
2015-12-09 13:19:46 +0100

    Besseres Datenbinding

--- monitor.js
+++ monitor.js
@@ -1,13 +1,37 @@
+"use strict";
+
 (function () {
     var MonitorComponent = ng
         .Component({
             selector: 'monitor',
-            templateUrl: 'monitor.html'
+            templateUrl: 'monitor.html',
+            directives: [ng.NgFor, ng.NgIf]
         })
         .Class({
             constructor: function () {
                 this.myName = "Alice";
-                this.names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
+                this.persons = [{
+                    name: 'Peter',
+                    age: 19
+                }, {
+                    name: 'Martin',
+                    age: 23
+                }, {
+                    name: 'Sarah',
+                    age: 32
+                }];
+
+                var persons = this.persons;
+                setInterval(function() {
+                    persons.push({
+                        name: 'Seppi',
+                        age: 56
+                    });
+                },1000);
+                setInterval(function() {
+                    var idx = parseInt(Math.random() * persons.length);
+                    persons[idx].age = persons[idx].age * 2;
+                },1500);
             }
         });
 
