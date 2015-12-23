cb77e7beeb16e467c61f9bdae903f4bc57dcebd9
tine2k <martin.maier.moessner@gmail.com>
2015-12-23 14:32:05 +0100

    Upgraded to Ang2Beta

--- monitor.js
+++ monitor.js
@@ -1,35 +1,32 @@
 "use strict";
 
 (function () {
-    var MonitorComponent = ng
-        .Component({
-            selector: 'monitor',
-            templateUrl: 'monitor.html',
-            directives: [ng.NgFor, ng.NgIf]
-        })
-        .Class({
-            constructor: [MyService, function (myService) {
-                this.myName = "Alice";
-                this.persons = myService.query(3);
+    var MonitorComponent = ng.core.Component({
+        selector: 'monitor',
+        templateUrl: 'monitor.html'
+    }).Class({
+        constructor: [MyService, function (myService) {
+            this.myName = "Alice";
+            this.persons = myService.query(3);
 
-                var that = this;
-                setInterval(function () {
-                    that.persons.push({
-                        name: 'Seppi',
-                        age: 56
-                    });
-                }, 1000);
-                setInterval(function () {
-                    var idx = parseInt(Math.random() * that.persons.length);
-                    that.persons[idx].age = that.persons[idx].age * 2;
-                }, 1500);
-            }],
-            getPersonCount: function () {
-                return this.persons.length;
-            }
-        });
+            var that = this;
+            setInterval(function () {
+                that.persons.push({
+                    name: 'Seppi',
+                    age: 56
+                });
+            }, 1000);
+            setInterval(function () {
+                var idx = parseInt(Math.random() * that.persons.length);
+                that.persons[idx].age = that.persons[idx].age * 2;
+            }, 1500);
+        }],
+        getPersonCount: function () {
+            return this.persons.length;
+        }
+    });
 
     document.addEventListener('DOMContentLoaded', function () {
-        ng.bootstrap(MonitorComponent, [MyService]);
+        ng.platform.browser.bootstrap(MonitorComponent, [MyService]);
     });
 }());
